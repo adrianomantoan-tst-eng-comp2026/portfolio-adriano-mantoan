e547735d70d22f544559f23d2fd415750389a35d
Adriano Mantoan <rute.feres@hotmail.coma>
2026-07-02 16:54:26 -0300

    feat: implementa listagem de tarefas com ações

--- engenharia-de-software/app.py
+++ engenharia-de-software/app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+tarefas = []
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", tarefas=tarefas)
+
+
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+    titulo = request.form.get("titulo")
+    descricao = request.form.get("descricao")
+
+    if titulo:
+        tarefa = {
+            "id": len(tarefas) + 1,
+            "titulo": titulo,
+            "descricao": descricao,
+            "status": "A Fazer"
+        }
+
+        tarefas.append(tarefa)
+
+    return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
